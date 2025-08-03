c5faeb0c6b62ce396e34203b25d73daab86fff0a
Fabiola2025-Mokuy <mokuyfabiola@gmail.com>
2025-08-03 10:21:55 +0200

    Portifolio1, Basic calculator

---
+++ Basic Calculator.py
@@ -0,0 +1,14 @@
+print ("Basic Calculator")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+sum_result = num1 + num2
+difference_result = num1 - num2
+product_result = num1 * num2
+quotient_result = num1 / num2
+
+print (num1, "+", num2, "=",sum_result)
+print (num1, "-", num2, "=", difference_result)
+print (num1, "*", num2, "=",product_result)
+print (num1, "/", num2, "=",quotient_result)
